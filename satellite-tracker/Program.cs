3c83e6cc223aad37ac23b730e0ad3c318233818b
GomezC2@iehp.org <GomezC2@iehp.org>
2020-08-16 21:10:57 -0700

    setting up project

---
+++ satellite-tracker/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace satellite_tracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //https://github.com/1manprojects/one_Sgp4
+        }
+    }
+}
